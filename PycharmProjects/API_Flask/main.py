292bcadf4fe1b35f14820c3ae3d66fbb0cad741d
Andrey <andrey@Mac-mini-admin.local>
2019-11-13 16:51:40 +0300

    up

--- PycharmProjects/API_Flask/main.py
+++ PycharmProjects/API_Flask/main.py
@@ -2,21 +2,21 @@ import yaml
 import psycopg2
 import pandas as pd
 from flask import Flask, request
-app = Flask(__name__)
 
 with open('configs/config.yml', 'r') as yaml_file:
     config = yaml.load(yaml_file, Loader=yaml.FullLoader)
 
 
-def get_tables_from(date_from, date_to):
+def get_tables_from(table_name, date_from, date_to):
 
     connect = psycopg2.connect(dbname=config['database_name'], user=config['database_user'],
                                password=config['database_password'], host=config['database_host'])
 
     cursor = connect.cursor()
 
-    cursor.execute("SELECT * FROM drugstore_positions "
-                   "WHERE updated_at BETWEEN %(date_from)s AND %(date_to)s "
+    sql_query = 'SELECT * FROM ' + table_name
+    cursor.execute(sql_query +
+                   " WHERE updated_at BETWEEN %(date_from)s AND %(date_to)s "
                    "ORDER BY updated_at DESC", {'date_from': date_from, 'date_to': date_to})
     drugstore_positions = cursor.fetchall()
 
@@ -26,6 +26,9 @@ def get_tables_from(date_from, date_to):
     return df.to_html()
 
 
+app = Flask(__name__)
+
+
 @app.route('/')
 def index_page():
     return 'Index page'
@@ -33,16 +36,18 @@ def index_page():
 
 @app.route('/catalogue_products')
 def catalogue_products_page():
+    table_name = 'catalogue_products'
     date_from = request.form['date_from']
     date_to = request.form['date_to']
-    return get_tables_from(date_from, date_to)
+    return get_tables_from(str(table_name), date_from, date_to)
 
 
 @app.route('/drugstore_positions')
 def drugstore_positions_page():
+    table_name = 'drugstore_positions'
     date_from = request.form['date_from']
     date_to = request.form['date_to']
-    return get_tables_from(date_from, date_to)
+    return get_tables_from(str(table_name), date_from, date_to)
 
 
 if __name__ == "__main__":
